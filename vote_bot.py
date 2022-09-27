f1af43a5d132859af94e8d65219946dc577f864f
jeremy <j2cunningham@yahoo.com>
2022-09-27 00:00:43 -0500

    vote

--- vote_bot.py
+++ vote_bot.py
@@ -6,7 +6,7 @@ from selenium.webdriver.common.by import By
 
 import time
 
-for x in range (100):
+for x in range (300):
     print(f'x is now {x}')
     driver = webdriver.Chrome("/usr/local/bin/chromedriver")
     driver.get('https://pantagraph.com/sports/high-school/football/vote-for-the-pantagraph-week-5-football-player-of-the-week/collection_930f2c64-3de6-11ed-86bb-5fbaa39f7510.html#anchor_item_7');
@@ -22,15 +22,14 @@ for x in range (100):
 
     time.sleep(2) # Let the user actually see something!
 
-    chk = driver.find_elements('xpath', "//input[@type='submit']")
+    try:
+        chk = driver.find_elements('xpath', "//input[@type='submit']")
+        button = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, "//*[@id='poll-c415b872-3dec-11ed-abd2-27648723ef1f-form']/button[1]")))
+        button.click()
+        time.sleep(2) # Let the user actually see something!
+    except:
+        pass
     # //*[@id="poll-c415b872-3dec-11ed-abd2-27648723ef1f-form"]/button[1]
-
-
-    button = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, "//*[@id='poll-c415b872-3dec-11ed-abd2-27648723ef1f-form']/button[1]")))
-    button.click()
-    time.sleep(2) # Let the user actually see something!
-
-
 
     driver.quit()
 
